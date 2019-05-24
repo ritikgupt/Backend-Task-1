0b04b20c036af013a2a7a035caf9728c72c28022
Ritik <ritikgupta89369@gmail.com>
2019-05-24 15:52:41 +0530

    Second Commit

---
+++ views/as.js
@@ -0,0 +1,4 @@
+console.log("Hey I am in the console.")
+var x=prompt("enter the string")
+y='actagt'
+a=y.length
---
+++ views/f.js
@@ -0,0 +1,4 @@
+<script>
+    console.log("Hey i am in the console");
+    prompt("Enter the strings");
+</script>
